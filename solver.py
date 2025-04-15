2214f06612722e449387558728c887b113de962d
denis <deniska.riga@gmail.com>
2025-04-15 21:31:11 +0200

    Set solver to default

--- solver.py
+++ solver.py
@@ -5,7 +5,7 @@ from branching import *
 
 is_graph_isomorphism_problem = False # True - Only to find equivalence classes, False - To count the number of automorphisms
 use_fast_colour_refinement = False # False - use basic colorref (works), True - use fast colorref (might not work on some instances)
-file = "SampleGraphsFastColorRefinement/threepaths160.gr" # substitute with a path to an instance here, file should be in the project folder and then navigated as usual
+file = "" # substitute with a path to an instance here, file should be in the project folder and then navigated as usual
 
 res, graphs = do_branching(file, use_fast_colour_refinement, is_graph_isomorphism_problem)
 
